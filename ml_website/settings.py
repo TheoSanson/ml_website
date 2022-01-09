f394e6bff5d3d13d652046485415d13b9cc8f62f
Theo Sanson <77782633+ZuluDoggo@users.noreply.github.com>
2022-01-09 18:51:08 +0800

    Update settings.py

--- ml_website/settings.py
+++ ml_website/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = "/var/www/wmsu-cet.herokuapp.com/static/"
+STATIC_ROOT = "https://wmsu-cet.herokuapp.com/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
