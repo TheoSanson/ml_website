b31a1af01557523498fa367f178f7f3cdc0393de
Theo Sanson <tjmileno@gmail.com>
2022-01-09 19:06:58 +0800

    Changes for Heroku

--- ml_website/settings.py
+++ ml_website/settings.py
@@ -126,7 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = "https://wmsu-cet.herokuapp.com/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -135,3 +136,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "user_dashboard"
 LOGOUT_REDIRECT_URL = "home"
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
